5e55b8e0fdfd162a6b4a1837682521ae72c80bd3
nzzhaozeen-svg <nz.zhaozeen@gmail.com>
2026-07-30 16:46:49 +0800

    Delete golang directory

--- golang/pkg/agentbay/interface/types.go
+++
@@ -1,4 +0,0 @@
-package interfaces
-
-// This file is intentionally minimal - we use types from the original packages
-// instead of duplicating them here to avoid code duplication and maintain consistency.
--- golang/tests/pkg/agentbay/config_test.go
+++
@@ -1,4 +0,0 @@
-package agentbay_test
-
-// Config loading tests removed - these test internal implementation details.
-// Configuration loading is tested through integration tests when creating AgentBay client.
